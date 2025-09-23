6ff798988224a23c374cbd0459652608cbee034e
david avila <avilarestrepos@gmail.com>
2025-09-23 18:04:52 -0500

    Cambios

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,3 +11,4 @@ root.render(
     <Welcome />
   </React.Fragment>
 )
+
--- frontend/src/pages/welcome.jsx
+++ frontend/src/pages/welcome.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import '../styles/welcome.css';
-import MisscookIcon from '../assets/MisscookOff.png';
+import React, { useState, useEffect } from 'react';
+  import '../styles/welcome.css';
+  import MisscookIcon from '../assets/MisscookOff.png';
 
-export function Welcome() {
-  return (
-    <div className="content">
-        <div className="welcome-icon-container">
-            <img src={MisscookIcon} className='welcome-icon' alt="Cooking Icon" />
-        </div>
-        <div className='welcome-title'>Generador de recetas AI</div>
-        <div className='welcome-text'>La mejor IA de Cocina. Introduce ingredientes y obtén recetas con nuestra IA de recetas.</div>
-        <div className="welcome-example">
-            Por ejemplo; desayuno con huevos y tomate
-        </div>
-        <button className='welcome-button'>Empezar</button>
-    </div>
-  );
-}
+  export function Welcome() {
+    const [apiMessage, setApiMessage] = useState('Cargando mensaje del servidor...');
+
+    useEffect(() => {
+      fetch('http://127.0.0.1:8000/') 
+        .then(response => response.json())
+        .then(data => setApiMessage(data.mensaje)) 
+        .catch(error => {
+          console.error("Error al conectar:", error);
+          setApiMessage("Error al conectar con el servidor.");
+        });
+    }, []); 
+
+    return (
+      <div className="content">
+          <div className="welcome-icon-container">
+              <img src={MisscookIcon} className='welcome-icon' alt="Cooking Icon" />
+          </div>
+          <div className='welcome-title'>Generador de recetas AI</div>
+
+          
+          <div className='welcome-text'>{apiMessage}</div>
+          
+          <div className="welcome-example">
+              Por ejemplo; desayuno con huevos y tomate
+          </div>
+          <button className='welcome-button'>Empezar</button>
+      </div>
+    );
+  }
